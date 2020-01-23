b9a9dd1147105912fa284fc5dc0ecdc0ee2e7245
Cruz Sanchez <cruzsanchez9209@gmail.com>
2020-01-22 21:33:29 -0600

    Initial Commit

---
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CarLotSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
